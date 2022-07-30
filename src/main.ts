087cd30cf70061d642351ffc00cd7e714291276a
Morris <Morris.Witthein@yahoo.de>
2022-07-30 12:20:12 +0200

    Remove boilerplate

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,8 @@ import { IntentsBitField } from "discord.js";
 import { Client } from "discordx";
 
 export const bot = new Client({
-  // To only use global commands (use @Guild for specific guild command), comment this line
   botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
 
-  // Discord intents
   intents: [
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMembers,
@@ -19,25 +17,13 @@ export const bot = new Client({
     IntentsBitField.Flags.GuildVoiceStates,
   ],
 
-  // Debug logs are disabled in silent mode
   silent: false,
 });
 
 bot.once("ready", async () => {
-  // Make sure all guilds are cached
   await bot.guilds.fetch();
 
-  // Synchronize applications commands with Discord
   await bot.initApplicationCommands();
-
-  // To clear all guild commands, uncomment this line,
-  // This is useful when moving from guild commands to global commands
-  // It must only be executed once
-  //
-  //  await bot.clearApplicationCommands(
-  //    ...bot.guilds.cache.map((g) => g.id)
-  //  );
-
   console.log("Bot started");
 });
 
@@ -50,19 +36,12 @@ bot.on("messageCreate", (message: Message) => {
 });
 
 async function run() {
-  // The following syntax should be used in the commonjs environment
-  //
-  // await importx(__dirname + "/{events,commands}/**/*.{ts,js}");
-
-  // The following syntax should be used in the ECMAScript environment
   await importx(dirname(import.meta.url) + "/{events,commands}/**/*.{ts,js}");
 
-  // Let's start the bot
   if (!process.env.DISCORD_TOKEN) {
     throw Error("Could not find BOT_TOKEN in your environment");
   }
 
-  // Log in with your bot token
   await bot.login(process.env.DISCORD_TOKEN);
 }
 
