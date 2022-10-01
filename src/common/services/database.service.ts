4eaeba660730a606cadc9f6672458ada9e43bad2
Frederik Adebahr <f.adebahr@gmail.com>
2022-10-01 11:11:10 +0200

    Rename env variable 'BUILDS_COLLECTION_NAME'

--- src/common/services/database.service.ts
+++ src/common/services/database.service.ts
@@ -8,13 +8,10 @@ export const collections: { builds?: mongoDB.Collection<ChampionBuildInformation
 export async function connectToDatabase() {
     dotenv.config();
     const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING ?? '');
-
     await client.connect();
 
     const db: mongoDB.Db = client.db(process.env.DB_NAME);
-
-    const buildsCollection = db.collection<ChampionBuildInformation>(process.env.BUILDS_COLLECTION_NAME ?? '');
-
+    const buildsCollection = db.collection<ChampionBuildInformation>(process.env.DB_BUILDS_COLLECTION_NAME ?? '');
     collections.builds = buildsCollection;
 
     console.log(
